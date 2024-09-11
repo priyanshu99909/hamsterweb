911d905924f1a3d66e15c09c873d06c77de60e74
priyanshu99909 <rawatpriyanshu70@gmail.com>
2024-09-11 20:07:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the button by its class name (returns a collection)
+  const loadPageBtn = document.getElementsByClassName("whitepaper-button");
+
+  // Since getElementsByClassName returns a collection, access the first element
+  if (loadPageBtn.length > 0) {
+    loadPageBtn[0].addEventListener("click", function () {
+      // Navigate to the new page (replace 'newpage.html' with your desired page URL)
+      window.location.href = "newpage.html";
+    });
+  }
+});
